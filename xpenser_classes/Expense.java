026cab268168ee28f82b4c4b8f6449913694dd5a
Katerina Tsikoura <147326236+katerinatsikoura@users.noreply.github.com>
2024-11-24 17:55:50 +0200

    java classes

---
+++ xpenser_classes/Expense.java
@@ -0,0 +1,92 @@
+package xpenser_classes;
+
+import java.util.*;
+
+public class Expense {
+    
+    private int expenseId;
+    private String date;
+    private double amount;
+    private String type;
+    private List<String> wbs;
+    private String receipt;
+    private List<String> status;
+    private String comments;
+
+    public Expense(int expenseId, String date, double amount, String type, List<String> wbs, String receipt, List<String> status, String comments) {
+        this.expenseId = expenseId;
+        this.date = date;
+        this.amount = amount;
+        this.type = type;
+        this.wbs = wbs;
+        this.receipt = receipt;
+        this.status = status;
+        this.comments = comments;
+    }
+
+    // Getters and Setters
+    public int getExpenseId() {
+        return expenseId;
+    }
+
+    public void setExpenseId(int expenseId) {
+        this.expenseId = expenseId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<String> getWbs() {
+        return wbs;
+    }
+
+    public void setWbs(List<String> wbs) {
+        this.wbs = wbs;
+    }
+
+    public String getReceipt() {
+        return receipt;
+    }
+
+    public void setReceipt(String receipt) {
+        this.receipt = receipt;
+    }
+
+    public List<String> getStatus() {
+        return status;
+    }
+
+    public void setStatus(List<String> status) {
+        this.status = status;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+}
---
+++ xpenser_classes/Expensesheet.java
@@ -0,0 +1,61 @@
+package xpenser_classes;
+
+import java.util.List;
+
+public class Expensesheet {
+    
+    private int expensesheetId;
+    private int userId;
+    private String date;
+    private List<Expense> expenseList;
+    private List<String> status;
+
+    public Expensesheet(int expensesheetId, int userId, String date, List<Expense> expenseList, List<String> status) {
+        this.expensesheetId = expensesheetId;
+        this.userId = userId;
+        this.date = date;
+        this.expenseList = expenseList;
+        this.status = status;
+    }
+
+    // Getters and Setters
+    public int getExpensesheetId() {
+        return expensesheetId;
+    }
+
+    public void setExpensesheetId(int expensesheetId) {
+        this.expensesheetId = expensesheetId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public List<Expense> getExpenseList() {
+        return expenseList;
+    }
+
+    public void setExpenseList(List<Expense> expenseList) {
+        this.expenseList = expenseList;
+    }
+
+    public List<String> getStatus() {
+        return status;
+    }
+
+    public void setStatus(List<String> status) {
+        this.status = status;
+    }
+}
